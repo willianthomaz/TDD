d018b6bb748bb7fdf1f83ed683d0205ae792dc55
Willian Thomaz <willian_thomaz@outlook.com>
2021-05-22 21:03:11 -0300

    Refatoração usando @Runwith e @Mock.Adicionando método Mockito.verify e Mockito.inOrder

--- src/test/java/com/willianThomaz/MockitoTest.java
+++ src/test/java/com/willianThomaz/MockitoTest.java
@@ -2,22 +2,40 @@ package com.willianThomaz;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MockitoTest {
+
+    @Mock
+    List<String> list;
 
     @Test
     public void primeiroTestMockito(){
-        List<String> list = Mockito.mock(ArrayList.class);
+//        Mockito.when(list.size()).thenReturn(2);
 
-        Mockito.when(list.size()).thenReturn(20);
+//       int size = 0;
+//       if (1 == 1){
+//           size = list.size();
+//           size = list.size();
+//       }
+        list.size();
+        list.add("");
 
-        int size = list.size();
+        InOrder inOrder = Mockito.inOrder(list);
+        inOrder.verify(list).size();
+        inOrder.verify(list).add("");
 
-        Assertions.assertThat(size).isEqualTo(20);
+
+//        Mockito.verify(list).size();
+//        Mockito.verify(list, Mockito.times(1)).size();
+//        Assertions.assertThat(size).isEqualTo(2);
 
     }
 }
